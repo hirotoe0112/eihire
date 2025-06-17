a658100badcc9026c38e8b1507aedbf3fe9fbc1b
hirotoe0112 <hirotoe0112@gmail.com>
2025-06-17 23:33:55 +0900

    画像追加

--- js/config.js
+++ js/config.js
@@ -1,8 +1,28 @@
 // パズル画像設定ファイル
 const PUZZLE_CONFIG = {
     images: [
+        'assets/images/puzzles/1.jpg',
+        'assets/images/puzzles/2.jpg',
         'assets/images/puzzles/3.jpg',
-        'assets/images/puzzles/6.jpg'
+        //'assets/images/puzzles/4.jpg',
+        //'assets/images/puzzles/5.jpg',
+        'assets/images/puzzles/6.jpg',
+        //'assets/images/puzzles/7.jpg',
+        //'assets/images/puzzles/8.jpg',
+        //'assets/images/puzzles/9.jpg',
+        'assets/images/puzzles/10.jpg',
+        //'assets/images/puzzles/11.jpg',
+        //'assets/images/puzzles/12.jpg',
+        //'assets/images/puzzles/13.jpg',
+        'assets/images/puzzles/14.jpg',
+        //'assets/images/puzzles/15.jpg',
+        //'assets/images/puzzles/16.jpg',
+        //'assets/images/puzzles/17.jpg',
+        //'assets/images/puzzles/18.jpg',
+        //'assets/images/puzzles/19.jpg',
+        //'assets/images/puzzles/20.jpg',
+        //'assets/images/puzzles/21.jpg',
+        //'assets/images/puzzles/22.jpg'
         // 新しい画像を追加する場合は、ここに追加してください
         // 例: 'assets/images/puzzles/image2.jpg',
         //     'assets/images/puzzles/image3.jpg'
